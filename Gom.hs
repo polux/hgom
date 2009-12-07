de08b8af1ee2439dfccb11e15be87201d4704a77
Paul Brauner <paul.brauner@inria.fr>
2009-12-07 14:13:12 +0100

    set my mind on getOpt

--- Gom.hs
+++ Gom.hs
@@ -11,7 +11,6 @@ import Gom.Java
 import System.Environment(getArgs)
 import Text.PrettyPrint.Leijen (pretty)
 import Control.Monad(liftM)
---import System.Console.CmdArgs
 import System.Console.GetOpt
     
 data Flag = FHelp
@@ -19,43 +18,34 @@ data Flag = FHelp
           | FPackage String 
        deriving (Eq,Show)
 
-{-
-data Config = Config {
-    version :: Bool,
-    packagePrefix :: String,
-    inputFile :: String
-  }
-
-m = mode $ Config {
-      version = def &= text "show version number",
--}    
-  
 options :: [OptDescr Flag]
 options =
   [Option []    ["help"]    (NoArg FHelp)                   "show this message",
    Option ['V'] ["version"] (NoArg FVersion)                "show version number",
    Option ['p'] ["package"] (ReqArg FPackage "packageName") "specify package name"]
-    
+
+usage :: String
+usage = "Usage: hgom [OPTION...] file"
+ 
 compilerOpts :: [String] -> IO ([Flag], [String])
 compilerOpts argv = 
   case getOpt Permute options argv of
     (o,n,[]  ) -> return (o,n)
-    (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
-  where header = "Usage: hgom [OPTION...] file"
+    (_,_,errs) -> ioError (userError (concat errs ++ usageInfo usage options))
 
-usage :: String -> IO ()
-usage h = putStrLn (usageInfo h options)
+printUsage :: String -> IO ()
+printUsage h = putStrLn (usageInfo h options)
 
 main ::  IO ()
 main = do (flags,n) <- getArgs >>= compilerOpts
           if FHelp `elem` flags 
-           then usage ""
+           then printUsage usage
            else if FVersion `elem` flags
-            then putStrLn "0.1"
+            then putStrLn "version 0.1 - december 2009 - copyrigth INRIA"
             else case n of
              [f] -> go f
-             []  -> usage "No input file specified.\n"
-             _   -> usage "Too many input files specified.\n"
+             []  -> printUsage "No input file specified.\n"
+             _   -> printUsage "Too many input files specified.\n"
 
 go :: String -> IO ()
 go f = do sig <- parseModule `liftM` readFile f
